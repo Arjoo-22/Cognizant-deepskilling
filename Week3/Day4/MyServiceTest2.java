4e33f8eafdebbc5536a40b084070b1d89209848f
arjoo22 <arjoo331@gmail.com>
2026-07-09 21:22:07 +0530

    Verifying Interactions

---
+++ Week3/Day4/MyServiceTest2.java
@@ -0,0 +1,35 @@
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+interface ExternalApi {
+    String getData();
+}
+
+class MyService {
+
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
