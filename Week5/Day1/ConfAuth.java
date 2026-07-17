f9477f5053858f0afbe5201d53406a086d894584
arjoo22 <arjoo331@gmail.com>
2026-07-17 18:32:17 +0530

    Configuring Authorization Servers and Resource Servers

---
+++ Week5/Day1/ConfAuth.java
@@ -0,0 +1,44 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class ConfAuth {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise2.class, args);
+    }
+
+    @Configuration
+    @LoadBalancerClient(name = "example-service")
+    static class LoadBalancerConfiguration {
+
+        @Bean
+        ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(
+                Environment environment,
+                LoadBalancerClientFactory factory) {
+
+            String name = environment.getProperty(
+                    LoadBalancerClientFactory.PROPERTY_NAME);
+
+            return new RandomLoadBalancer(
+                    factory.getLazyProvider(
+                            name,
+                            ServiceInstanceListSupplier.class),
+                    name);
+        }
+    }
+}
+
