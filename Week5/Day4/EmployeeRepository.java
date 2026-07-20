885ec87bda30db606b2948897c6d8ad960356445
arjoo22 <arjoo331@gmail.com>
2026-07-20 14:30:32 +0530

    queries on stock table using Query Methods

---
+++ Week5/Day4/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.example.ormlearn.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.ormlearn.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e FROM Employee e LEFT JOIN FETCH e.department LEFT JOIN FETCH e.skillList WHERE e.permanent = true")
+    List<Employee> getAllPermanentEmployees();
+}
